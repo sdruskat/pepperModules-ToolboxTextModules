c089b18dfcdb98c1f14f06b6d0e014d7ad98b418
Stephan Druskat <stephan.druskat@uni-jena.de>
2016-06-15 15:08:53 +0200

    Fix copyrights

--- pepperModules-ToolboxTextModules/src/main/java/org/corpus_tools/peppermodules/toolbox/text/ToolboxTextMapper.java
+++ pepperModules-ToolboxTextModules/src/main/java/org/corpus_tools/peppermodules/toolbox/text/ToolboxTextMapper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2016 Stephan Druskat
+ * Copyright 2016 Humboldt-Universität zu Berlin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
